14f503e9c35f2b1c3bc1e891ce236ed36ef63ba0
Mar-cybe <67813877+Mar-cybe@users.noreply.github.com>
2020-07-10 03:14:40 -0600

    Siguiendo con mi nuevo repositorio en github

---
+++ src/main/java/hn/uth/poo/p2/objetos/ISV.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.uth.poo.p2.objetos;
+
+/**
+ *
+ * @author MARLON HERNANDEZ
+ */
+public abstract class ISV implements IISV{
+    protected int Id;
+    protected String Nombre;
+
+    public ISV() {
+    }
+
+    public ISV(int Id, String Nombre) {
+        this.Id = Id;
+        this.Nombre = Nombre;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+}
